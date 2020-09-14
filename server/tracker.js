054414f614e1e1c51d6e51ae8b6e77ab9ed87113
Badtz <badtzmaru139@gmail.com>
2020-09-13 23:20:17 -0700

    handle no users on cleanup

--- server/tracker.js
+++ server/tracker.js
@@ -120,22 +120,24 @@ function cleanup() {
   firebase.database().ref('users').once('value', (snapshot) => {
     const users = snapshot.val();
     const leaveTime = Date.now();
-    Object.keys(users).forEach((userKey) => {
-      const user = users[userKey];
-      if (user.joined) {
-        let time;
-        if (user.time) {
+    if (users) {
+      Object.keys(users).forEach((userKey) => {
+        const user = users[userKey];
+        if (user.joined) {
+          let time;
+          if (user.time) {
           // eslint-disable-next-line prefer-destructuring
-          time = user.time;
-        } else {
-          time = 0;
+            time = user.time;
+          } else {
+            time = 0;
+          }
+          firebase.database().ref(`users/${userKey}`).update({
+            joined: null,
+            time: time + (leaveTime - user.joined),
+          });
         }
-        firebase.database().ref(`users/${userKey}`).update({
-          joined: null,
-          time: time + (leaveTime - user.joined),
-        });
-      }
-    });
+      });
+    }
     console.log('\x1b[32m%s\x1b[0m', 'Cleanup complete');
     connect();
   });
